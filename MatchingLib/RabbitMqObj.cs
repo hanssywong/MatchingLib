14920f7249a284aee86a7e64260dfeefbec390ea
Hans Wong <hans.sy.wong@qq.com>
2018-03-01 18:36:26 +0800

    Added RabbitMq to lib

---
+++ MatchingLib/RabbitMqObj.cs
@@ -0,0 +1,72 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchingLib
+{
+    public class RabbitMqOut
+    {
+        private ConnectionFactory factory { get; } = new ConnectionFactory();
+        private IConnection conn { get; set; }
+        private IModel channel { get; set; }
+        private string queueName { get; set; }
+
+        public RabbitMqOut(string uri, string queue_name)
+        {
+            factory.Uri = new Uri(uri);
+            conn = factory.CreateConnection();
+            channel = conn.CreateModel();
+            queueName = queue_name;
+        }
+
+        public void Enqueue(byte[] data)
+        {
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+            channel.BasicPublish(exchange: "",
+                                 routingKey: queueName,
+                                 basicProperties: properties,
+                                 body: data);
+        }
+    }
+    public class RabbitMqIn
+    {
+        private ConnectionFactory factory { get; } = new ConnectionFactory();
+        private IConnection conn { get; set; }
+        private IModel channel { get; set; }
+        private EventingBasicConsumer consumer { get; set; }
+        private string queueName { get; set; }
+
+        public RabbitMqIn(string uri, string queue_name)
+        {
+            factory.Uri = new Uri(uri);
+            conn = factory.CreateConnection();
+            channel = conn.CreateModel();
+            queueName = queue_name;
+            channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            consumer = new EventingBasicConsumer(channel);
+            //consumer.Received += (model, ea) =>
+            //{
+            //    var body = ea.Body;
+            //    var message = Encoding.UTF8.GetString(body);
+            //    Console.WriteLine(" [x] Received {0}", message);
+            //};
+        }
+
+        public void BindReceived(EventHandler<BasicDeliverEventArgs> handler)
+        {
+            consumer.Received += handler;
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+        }
+
+        public void MsgFinished(BasicDeliverEventArgs ea)
+        {
+            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+        }
+    }
+}
